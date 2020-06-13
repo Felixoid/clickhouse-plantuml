a5a0fa7cfdd2ca742b91039a5eb0a670f006a930
Mikhail f. Shiryaev <mr.felixoid@gmail.com>
2020-06-14 01:36:59 +0200

    100% coverage for plantuml.py

--- tests/test_plantuml.py
+++ tests/test_plantuml.py
@@ -3,6 +3,11 @@ from unittest.mock import patch
 from clickhouse_plantuml import plantuml as p
 
 
+class DummyColumn(p.Column):
+    def __init__(self):
+        pass
+
+
 class TestPlantuml(unittest.TestCase):
     def setUp(self):
         self.test_table_data = {
@@ -10,16 +15,24 @@ class TestPlantuml(unittest.TestCase):
             "name": "test_table",
             "dependencies": ["database.table"],
             "create_table_query": "SOME LONG STRING",
-            "engine": "MergeTree",
-            "engine_full": "MergeTree() PARTITION BY date ORDER BY date",
+            "engine": "ReplicatedReplacingMergeTree",
+            "engine_full": (
+                "ReplicatedReplacingMergeTree('/zk/node', 'replica_name', "
+                "'ver') PARTITION BY date ORDER BY date"
+            ),
             "partition_key": "date",
             "sorting_key": "date",
             "primary_key": "date",
             "sampling_key": "",
         }
         self.test_table = p.Table(**self.test_table_data)
+        self.test_table.parse_engine()
         self.test_tables = p.Tables(None)
         self.test_tables.append(self.test_table)
+
+    def tearDown(self):
+        del self.test_table
+        del self.test_tables
 
     def test_plantuml(self):
         assert p.plantuml([]) == p.plantuml_header() + p.plantuml_footer()
@@ -40,21 +53,102 @@ class TestPlantuml(unittest.TestCase):
             "\n"
         )
 
-    def test_gen_tables(self):
+    @patch.object(p, "gen_dependencies", return_value="mocked_dependencies")
+    @patch.object(p, "gen_table", return_value="mocked_table")
+    def test_gen_tables(self, mock_table, mock_dependencies):
         # The substatements will be testet separately
-        assert p.gen_tables([]) == ""
-        with patch.object(p, "gen_table", return_value="") as mock:
-            assert p.gen_tables(self.test_tables) == ""
-            mock.assert_called_once_with(self.test_table)
+        assert p.gen_tables([]) == "mocked_dependencies"
+        assert p.gen_tables(self.test_tables) == (
+            "mocked_table" "mocked_dependencies"
+        )
+        mock_table.assert_called_once_with(self.test_table)
+        mock_dependencies.assert_called_with(self.test_tables)
 
     def test_plantuml_footer(self):
         assert p.plantuml_footer() == "@enduml\n"
+
+    @patch.object(p, "gen_table_columns", return_value="mocked_columns")
+    @patch.object(p, "gen_table_engine", return_value="mocked_engine")
+    def test_gen_table(self, mock_engine, mock_columns):
+        assert p.gen_table(self.test_table) == (
+            "Table(test_database.test_table) {\n"
+            "mocked_engine"
+            "mocked_columns"
+            "}\n\n"
+        )
+        mock_engine.assert_called_once_with(self.test_table)
+        mock_columns.assert_called_once_with(self.test_table)
+
+    def test_gen_dependencies(self):
+        another_table = dict(self.test_table_data)
+        another_table.update(
+            {
+                "database": "database",
+                "name": "table",
+                "dependencies": ["test_database.test_table"],
+            }
+        )
+        tables = self.test_tables
+        tables.append(p.Table(**another_table))
+        tables[0].rev_dependencies = ["test_database.test_table"]
+        tables[1].rev_dependencies = ["database.table"]
+        assert p.gen_dependencies(tables) == (
+            "test_database.test_table -|> database.table\n"
+            "test_database.test_table -|> test_database.test_table\n"
+            "database.table -|> test_database.test_table\n"
+            "database.table -|> database.table\n"
+        )
 
     def test_table_macros(self):
         assert p.table_macros("MaterializedView") == "MaterializedView"
         assert p.table_macros("View") == "View"
         assert p.table_macros("Distributed") == "Distributed"
         assert p.table_macros("SomeRandEngine") == "Table"
+
+    def test_gen_table_engine(self):
+        assert p.gen_table_engine(self.test_table) == (
+            "ENGINE=**ReplicatedReplacingMergeTree**\n"
+            "..engine config..\n"
+            "version: ver\n"
+            "..replication..\n"
+            "zoo_path: /zk/node\n"
+            "replica: replica_name\n"
+        )
+
+    @patch.object(p, "column_keys", return_value="")
+    def test_gen_table_column(self, mock_column_keys):
+        col_date = DummyColumn()
+        col_date.__dict__.update(
+            {
+                "name": "date",
+                "type": "Date",
+                "database": "test_database",
+                "table": "test_table",
+            }
+        )
+        col_str = DummyColumn()
+        col_str.__dict__.update(
+            {
+                "name": "str",
+                "type": "String",
+                "database": "test_database",
+                "table": "test_table",
+            }
+        )
+        self.test_table.add_column(col_date)
+        self.test_table.add_column(col_str)
+        self.test_table.sorting_key = "date, str"
+        assert p.gen_table_columns(self.test_table) == (
+            "==columns==\n"
+            "date: Date\n"
+            "str: String\n"
+            "..<size:15><&list-rich></size>partition key..\n"
+            "date\n"
+            "..<size:15><&signal></size>sorting key..\n"
+            "date, str\n"
+            "..<size:15><&key></size>primary key..\n"
+            "date\n"
+        )
 
     def test_key_sign(self):
         assert p.key_sign("any random thing") == ""
@@ -63,3 +157,17 @@ class TestPlantuml(unittest.TestCase):
         assert p.key_sign("primary") == sign.format("key")
         assert p.key_sign("sampling") == sign.format("collapse-down")
         assert p.key_sign("sorting") == sign.format("signal")
+
+    def test_column_keys(self):
+        col = DummyColumn()
+        keys = ["partition", "sorting", "primary", "sampling", "noize"]
+        for key in keys:
+            setattr(col, "is_in_{}_key".format(key), True)
+
+        assert p.column_keys(col, keys) == (
+            " <size:15><&list-rich></size>"
+            " <size:15><&signal></size>"
+            " <size:15><&key></size>"
+            " <size:15><&collapse-down></size>"
+            " "
+        )
