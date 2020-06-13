940ad79cf260f9b2bb0c8d414c375296e8996f95
Mikhail f. Shiryaev <mr.felixoid@gmail.com>
2020-06-13 21:05:02 +0200

    Improve plantuml.py and tests for it

--- clickhouse_plantuml/plantuml.py
+++ clickhouse_plantuml/plantuml.py
@@ -3,7 +3,7 @@
 # License: Apache-2.0
 # Copyright (C) 2020 Mikhail f. Shiryaev
 
-from . import Tables, Column
+from . import Column, Table, Tables
 from typing import List
 
 
@@ -17,6 +17,8 @@ def plantuml_header():
     header = "\n".join(
         (
             "@startuml",
+            "' This diagram is generated with "
+            "https://github.com/Felixoid/clickhouse-plantuml",
             "!define Table(x) class x << (T,mistyrose) >>",
             "!define View(x) class x << (V,lightblue) >>",
             "!define MaterializedView(x) class x << (m,orange) >>",
@@ -38,37 +40,32 @@ def gen_tables(tables: Tables):
     """
     code = ""
     for t in tables:
-        include_primary = t.sorting_key != t.primary_key
-        if include_primary:
-            table_keys = ["partition", "sorting", "primary", "sampling"]
-        else:
-            table_keys = ["partition", "sorting", "sampling"]
+        code += gen_table(t)
 
-        code += "{}({})".format(table_macros(t.engine), str(t)) + " {\n"
+    code += gen_dependencies(tables)
+    return code
 
-        code += "ENGINE=**{}**\n".format(t.engine)
-        if t.engine_config:
-            code += "..engine config..\n"
-        for k, v in t.engine_config:
-            code += "{}: {}\n".format(k, v)
 
-        if t.replication_config:
-            code += "..replication..\n"
-        for k, v in t.replication_config:
-            code += "{}: {}\n".format(k, v)
+def plantuml_footer():
+    return "@enduml\n"
 
-        code += "==columns==\n"
-        for c in t.columns:
-            code += "{}: {}".format(c.name, c.type)
-            code += column_keys(c, table_keys)
-            code += "\n"
 
-        for k in table_keys:
-            key_string = getattr(t, "{}_key".format(k))
-            if key_string:
-                code += "..{}{} key..\n{}\n".format(key_sign(k), k, key_string)
+def gen_table(table: Table) -> str:
+    t = table
+    # Table header
+    code = "{}({}) {{\n".format(table_macros(t.engine), str(t))
 
-        code += "}\n"
+    code += gen_table_engine(t)
+    code += gen_table_columns(t)
+
+    # Table footer
+    code += "}\n\n"
+    return code
+
+
+def gen_dependencies(tables: Tables) -> str:
+    code = ""
+    for t in tables:
         code += "".join(
             "{} -|> {}\n".format(str(t), d)
             for d in t.dependencies
@@ -80,20 +77,48 @@ def gen_tables(tables: Tables):
             for r in t.rev_dependencies
             if r in tables.as_dict
         )
-
-        code += "\n"
-
     return code
-
-
-def plantuml_footer():
-    return "@enduml\n"
 
 
 def table_macros(table_type: str):
     if table_type in ("MaterializedView", "View", "Distributed"):
         return table_type
     return "Table"
+
+
+def gen_table_engine(table: Table) -> str:
+    t = table
+    code = "ENGINE=**{}**\n".format(t.engine)
+    if t.engine_config:
+        code += "..engine config..\n"
+    for k, v in t.engine_config:
+        code += "{}: {}\n".format(k, v)
+
+    if t.replication_config:
+        code += "..replication..\n"
+    for k, v in t.replication_config:
+        code += "{}: {}\n".format(k, v)
+
+    return code
+
+
+def gen_table_columns(table: Table) -> str:
+    t = table
+    table_keys = ["partition", "sorting", "sampling"]
+    if t.sorting_key != t.primary_key:
+        # If primary != sorting, it's worth to append it
+        table_keys.insert(2, "primary")
+
+    code = "==columns==\n"
+    for c in t.columns:
+        code += "{}: {}{}\n".format(c.name, c.type, column_keys(c, table_keys))
+
+    for k in table_keys:
+        key_string = getattr(t, "{}_key".format(k))
+        if key_string:
+            code += "..{}{} key..\n{}\n".format(key_sign(k), k, key_string)
+
+    return code
 
 
 def key_sign(key: str) -> str:
@@ -110,9 +135,9 @@ def key_sign(key: str) -> str:
 
 
 def column_keys(column: Column, table_keys: List[str]) -> str:
-    keys = ""
+    code = ""
 
     for key in table_keys:
         if getattr(column, "is_in_{}_key".format(key)):
-            keys += " {}".format(key_sign(key))
-    return keys
+            code += " {}".format(key_sign(key))
+    return code
--- tests/test_plantuml.py
+++ tests/test_plantuml.py
@@ -1,11 +1,33 @@
 import unittest
-from clickhouse_plantuml import plantuml
+from clickhouse_plantuml import plantuml as p
 
 
 class TestPlantuml(unittest.TestCase):
+    def setUp(self):
+        self.test_table_data = {
+            "database": "test_database",
+            "name": "test_table",
+            "dependencies": ["database.table"],
+            "create_table_query": "SOME LONG STRING",
+            "engine": "MergeTree",
+            "engine_full": "MergeTree() PARTITION BY date ORDER BY date",
+            "partition_key": "date",
+            "sorting_key": "date",
+            "primary_key": "date",
+            "sampling_key": "",
+        }
+        self.test_table = p.Table(**self.test_table_data)
+        self.test_tables = p.Tables(None)
+        self.test_tables.append(self.test_table)
+
+    def test_plantuml(self):
+        assert p.plantuml([]) == p.plantuml_header() + p.plantuml_footer()
+
     def test_plantuml_header(self):
-        assert plantuml.plantuml_header() == (
+        assert p.plantuml_header() == (
             "@startuml\n"
+            "' This diagram is generated with "
+            "https://github.com/Felixoid/clickhouse-plantuml\n"
             "!define Table(x) class x << (T,mistyrose) >>\n"
             "!define View(x) class x << (V,lightblue) >>\n"
             "!define MaterializedView(x) class x << (m,orange) >>\n"
@@ -17,11 +39,23 @@ class TestPlantuml(unittest.TestCase):
             "\n"
         )
 
+    def test_gen_tables(self):
+        # The substatements will be testet separately
+        assert p.gen_tables([]) == ""
+
     def test_plantuml_footer(self):
-        assert plantuml.plantuml_footer() == "@enduml\n"
+        assert p.plantuml_footer() == "@enduml\n"
 
     def test_table_macros(self):
-        assert plantuml.table_macros("MaterializedView") == "MaterializedView"
-        assert plantuml.table_macros("View") == "View"
-        assert plantuml.table_macros("Distributed") == "Distributed"
-        assert plantuml.table_macros("SomeRandEngine") == "Table"
+        assert p.table_macros("MaterializedView") == "MaterializedView"
+        assert p.table_macros("View") == "View"
+        assert p.table_macros("Distributed") == "Distributed"
+        assert p.table_macros("SomeRandEngine") == "Table"
+
+    def test_key_sign(self):
+        assert p.key_sign("any random thing") == ""
+        sign = "<size:15><&{}></size>"
+        assert p.key_sign("partition") == sign.format("list-rich")
+        assert p.key_sign("primary") == sign.format("key")
+        assert p.key_sign("sampling") == sign.format("collapse-down")
+        assert p.key_sign("sorting") == sign.format("signal")
